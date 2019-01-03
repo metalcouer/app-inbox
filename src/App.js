e2b03413248d32cbacc3c68b312c3f3a75f6009d
Diane Sanchez <diane@Dianes-MacBook-Air-2.local>
2019-01-03 15:32:11 -0700

    first commit, added message and tool bar components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div class="row toolbar">
+  <div class="col-md-12">
+    <p class="pull-right">
+      <span class="badge badge">2</span>
+      unread messages
+    </p>
+
+    <button class="btn btn-default">
+      <i class="fa fa-check-square-o"></i>
+    </button>
+
+    <button class="btn btn-default">
+      Mark As Read
+    </button>
+
+    <button class="btn btn-default">
+      Mark As Unread
+    </button>
+
+    <select class="form-control label-select">
+      <option>Apply label</option>
+      <option value="dev">dev</option>
+      <option value="personal">personal</option>
+      <option value="gschool">gschool</option>
+    </select>
+
+    <select class="form-control label-select">
+      <option>Remove label</option>
+      <option value="dev">dev</option>
+      <option value="personal">personal</option>
+      <option value="gschool">gschool</option>
+    </select>
+
+    <button class="btn btn-default">
+      <i class="fa fa-trash-o"></i>
+    </button>
+  </div>
+</div>
+    )
   }
 }
 
